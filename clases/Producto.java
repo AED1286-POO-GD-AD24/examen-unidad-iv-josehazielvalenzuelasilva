ddd633e1f4b5443cb1704c5f30e0ece7487ef20c
Jose Haziel Valenzuela Silva <l22170844@culiacan.tecnm.mx>
2024-11-11 17:01:16 +0000

    Terminado

--- clases/Producto.java
+++ clases/Producto.java
@@ -1,3 +1,23 @@
 // Clase abstracta Producto
 package clases;
+public abstract class Producto {
+    protected String nombre;
+    protected double precio;
+    
+    
+    public Producto() {
+        this.nombre = "sin nombre";
+        this.precio = 0.0;
+    }
+
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public abstract String getNombre();
+    public abstract double getPrecio();
+     
+}
 
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -10,9 +10,26 @@ public class Principal {
     
             ProductoElectronico laptop = new ProductoElectronico("Laptop", 1200.00);
             ProductoAlimenticio manzana = new ProductoAlimenticio("Manzana", 0.50);
+            ProductoAlimenticio salchicha = new ProductoAlimenticio("salchicha", 5);
+            ProductoElectronico celular = new ProductoElectronico("celular", 4300.50);
+            ProductoAlimenticio mandarina = new ProductoAlimenticio("mandarina", 10);
+            ProductoElectronico television = new ProductoElectronico("television", 5000.20);
+            ProductoElectronico licuadora = new ProductoElectronico("licuadora", 450);
+            ProductoAlimenticio queso = new ProductoAlimenticio("queso", 60);
+            ProductoAlimenticio pera = new ProductoAlimenticio("pera", 15);
+            ProductoAlimenticio sandia =new ProductoAlimenticio("sandia", 40);
+
     
             carrito.agregarProducto(laptop);
             carrito.agregarProducto(manzana);
+            carrito.agregarProducto(salchicha);
+            carrito.agregarProducto(celular);
+            carrito.agregarProducto(mandarina);
+            carrito.agregarProducto(television);
+            carrito.agregarProducto(licuadora);
+            carrito.agregarProducto(queso);
+            carrito.agregarProducto(pera);
+            carrito.agregarProducto(sandia);
     
             carrito.mostrarProductos();
             System.out.println("Total: $" + carrito.calcularTotal());
